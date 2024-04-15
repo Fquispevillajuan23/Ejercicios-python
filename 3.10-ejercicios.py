46315f8fcc63c567f2ace04b6f69b44000a6b1e7
fredy <70190651@continental.edu.pe>
2024-04-15 15:22:00 -0500

    se modifico el archivo 3.10

--- 3.10-ejercicios.py
+++ 3.10-ejercicios.py
@@ -1,2 +1,12 @@
-#calcular el interes
+#calcular el ínteres
+print('Dame saldo actual: ' )
+saldo = float(input())
+
+if (saldo < 10000.00):
+    saldo = saldo*(1+0.03)
+
+else:
+    saldo = saldo*(1+0.04)
+    print("Saldo final es %5.2f"%saldo)
+
 
