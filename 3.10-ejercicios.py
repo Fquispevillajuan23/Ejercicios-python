dc7357636fd510a4f5865d26e26e1d983873572c
Fred23 <70190651@continental.edu.pe>
2024-04-16 19:20:15 -0500

    3.10 - mejorado

--- 3.10-ejercicios.py
+++ 3.10-ejercicios.py
@@ -1,12 +1,26 @@
-#calcular el ínteres
-print('Dame saldo actual: ' )
-saldo = float(input())
+# Definir la función para calcular el interés y el saldo final
+def calcular_saldo_final(saldo):
+    if saldo < 10000.00:
+        interes = saldo * 0.03
+        saldo_final = saldo + interes
+    else:
+        interes = saldo * 0.04
+        saldo_final = saldo + interes
+    return saldo_final
 
-if (saldo < 10000.00):
-    saldo = saldo*(1+0.03)
 
-else:
-    saldo = saldo*(1+0.04)
-    print("Saldo final es %5.2f"%saldo)
+# Inicio del programa
+def main():
+    # Pedir al usuario que ingrese el saldo
+    saldo = float(input("Ingrese el saldo de la cuenta: $"))
+
+    # Calcular el saldo final llamando a la función calcular_saldo_final
+    saldo_final = calcular_saldo_final(saldo)
+
+    # Mostrar el saldo final al usuario
+    print("El saldo final después de un año es: ${:.2f}".format(saldo_final))
 
 
+# Llamar a la función main para ejecutar el programa
+if __name__ == "__main__":
+    main()
